ee6fc5a6ed26e53f001036178ec77c4586c2c467
zk <zhukui.1998@qq.com>
2022-12-05 16:11:53 +0800

    cvm

--- Ch05/dologregres.py
+++ Ch05/dologregres.py
@@ -1,6 +1,6 @@
 import logRegres
 
-# dataarr, labelmat = logRegres.loadDataSet()
+dataarr, labelmat = logRegres.loadDataSet()
 # # print logRegres.gradAscent(dataarr, labelmat)
 # weights = logRegres.gradAscent(dataarr, labelmat)
 # logRegres.plotBestFit(weights.getA())
